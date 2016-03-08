f85707740f446cf40a7da021a524ffcb171f22c9
Sage.Smith <sagesmith@LS2-00204.local>
2016-03-08 16:48:34 -0700

    Fix errors caused in mergs

--- tests/unit_tests/util/test_facts.py
+++ tests/unit_tests/util/test_facts.py
@@ -628,14 +628,14 @@ class TestFactsAreValid(unittest.TestCase):
         Tests to make sure that a valid fact still works
         """
         fact = Mock(decimals='-4', value='869098', xvalue = 869098, precision = None)
-        self.assertTrue(fact_lib.fact_components_valid(fact))
+        self.assertTrue(fact_lib._fact_components_valid(fact))
 
     def test_fact_components_valid_on_none_type_fact(self):
         """
         Tests to make sure that a None type fact is not valid
         """
         fact = None
-        self.assertFalse(fact_lib.fact_components_valid(fact))
+        self.assertFalse(fact_lib._fact_components_valid(fact))
 
     def test_fact_components_valid_on_none_type_context(self):
         """
@@ -643,7 +643,7 @@ class TestFactsAreValid(unittest.TestCase):
         """
         fact = Mock(decimals='-1', value='670', xValue=670, precision=None)
         fact.context = None
-        self.assertFalse(fact_lib.fact_components_valid(fact))
+        self.assertFalse(fact_lib._fact_components_valid(fact))
 
     def test_fact_components_valid_on_none_type_segDimValue(self):
         """
@@ -651,4 +651,4 @@ class TestFactsAreValid(unittest.TestCase):
         """
         fact = Mock(decimals='-2', value='6500', xValue=6500, precision=None)
         fact.context.segDimValues = None
-        self.assertFalse(fact_lib.fact_components_valid(fact))
+        self.assertFalse(fact_lib._fact_components_valid(fact))
