e3fb7d166fc2a7c3c8ea8ece74a45fe31faa4ec6
Sage.Smith <sagesmith@LS2-00204.local>
2016-03-14 14:47:16 -0600

    Fix flake8 errors

--- dqc_us_rules/dqc_us_0033_0036.py
+++ dqc_us_rules/dqc_us_0033_0036.py
@@ -80,7 +80,9 @@ def _doc_period_end_date_check(model_xbrl):
         # DocumentPeriodEndDate
         for lea_key, fact_group in dei_facts.items():
             eop_fact = dped_facts.get(lea_key, default_dped_fact)[0]
-            if eop_fact is None or eop_fact.context is None or eop_fact.context.endDatetime is None:
+            if eop_fact is None or \
+               eop_fact.context is None or \
+               eop_fact.context.endDatetime is None:
                 continue
 
             # Arelle adjusts context end date to end-of-day midnight
