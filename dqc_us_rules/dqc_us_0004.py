3e487a635b747f8da0a612d518d41e488d028aa6
Sage.Smith <sagesmith@LS2-00204.local>
2016-03-14 10:56:26 -0600

    Fix Flake8 errors in 0004

--- dqc_us_rules/dqc_us_0004.py
+++ dqc_us_rules/dqc_us_0004.py
@@ -11,24 +11,43 @@ _LIABILITIES_CONCEPT = 'LiabilitiesAndStockholdersEquity'
 _CODE_NAME = 'DQC.US.0004'
 _RULE_VERSION = '1.0'
 
+
 def assets_eq_liability_equity(val):
     """
     Assets equals Liabilities and Stockholders Equity
+
+    :param val: val to look at the modelXbrl for
+    :type val: val
     """
-    for fact_assets, fact_liabilities in _assets_eq_liability_equity(val.modelXbrl):
-        val.modelXbrl.error('{}.16'.format(_CODE_NAME), messages.get_message(_CODE_NAME),
-                            modelObject=[fact_assets, fact_liabilities], ruleVersion=_RULE_VERSION)
+    for fact in _assets_eq_liability_equity(val.modelXbrl):
+        fact_assets, fact_liabilities = fact
+        val.modelXbrl.error(
+            '{}.16'.format(_CODE_NAME),
+            messages.get_message(_CODE_NAME),
+            modelObject=[fact_assets, fact_liabilities],
+            ruleVersion=_RULE_VERSION
+        )
 
 
-def _assets_eq_liability_equity(modelXbrl):
-    assets_concept = modelXbrl.nameConcepts[_ASSETS_CONCEPT][0] if modelXbrl.nameConcepts[_ASSETS_CONCEPT] else None
-    liability_equity_concept = modelXbrl.nameConcepts[_LIABILITIES_CONCEPT][0] if modelXbrl.nameConcepts[_LIABILITIES_CONCEPT] else None
+def _assets_eq_liability_equity(model_xbrl):
+    """
+    Yeilds fact assets and fact liablities as long as it is able to
+
+    :param model_xbrl: modelXbrl to check name concepts of
+    :type model_xbrl: ModelXbrl
+    """
+    assets_concept = model_xbrl.nameConcepts[_ASSETS_CONCEPT][0] \
+        if model_xbrl.nameConcepts[_ASSETS_CONCEPT] else None
+
+    liability_equity_concept = model_xbrl.nameConcepts[_LIABILITIES_CONCEPT][0]\
+        if model_xbrl.nameConcepts[_LIABILITIES_CONCEPT] else None
 
     if assets_concept is not None and liability_equity_concept is not None:
-        assets_facts = modelXbrl.factsByQname[assets_concept.qname]
-        liability_equity_facts = modelXbrl.factsByQname[liability_equity_concept.qname]
+        assets_facts = model_xbrl.factsByQname[assets_concept.qname]
+        liability_equity_facts = \
+            model_xbrl.factsByQname[liability_equity_concept.qname]
 
-        fact_dict = {}
+        fact_dict = dict()
         fact_dict[_ASSETS_CONCEPT] = assets_facts
         fact_dict[_LIABILITIES_CONCEPT] = liability_equity_facts
         fact_groups = facts.prepare_facts_for_calculation(fact_dict)
@@ -36,12 +55,17 @@ def _assets_eq_liability_equity(modelXbrl):
         for fact_group in fact_groups:
             fact_assets = fact_group[_ASSETS_CONCEPT]
             fact_liabilities = fact_group[_LIABILITIES_CONCEPT]
-            if fact_assets.context is not None and fact_assets.context.instantDatetime is not None:
+            if fact_assets.context is not None and \
+               fact_assets.context.instantDatetime is not None:
+
                 dec_assets = inferredDecimals(fact_assets)
                 dec_liabilities = inferredDecimals(fact_liabilities)
                 min_dec = min(dec_assets, dec_liabilities)
-                if _min_dec_valid(min_dec) and _values_unequal(fact_assets.xValue, fact_liabilities.xValue, min_dec):
+                if _min_dec_valid(min_dec) and _values_unequal(
+                        fact_assets.xValue, fact_liabilities.xValue, min_dec
+                ):
                     yield fact_assets, fact_liabilities
+
 
 def _min_dec_valid(min_dec):
     """
@@ -56,10 +80,22 @@ def _min_dec_valid(min_dec):
 
     return min_dec is not None and not isnan(min_dec)
 
+
 def _values_unequal(val1, val2, dec_scale, margin_scale=2):
     """
     Checks the values for equality based on their scaling.
     Returns False if the values are equal, otherwise True.
+
+    :param val1: first value to round
+    :type val1: val
+    :param val2: second value to round
+    :type val2: val
+    :param dec_scale: precision on rounded value
+    :type dec_scale: decimal
+    :param margin_scale: margin of scale for the margin of error
+    :type margin_scale: float
+    :rtype: bool
+    :return: True if the values are not equal
     """
     round_val1 = roundValue(val1, decimals=dec_scale)
     round_val2 = roundValue(val2, decimals=dec_scale)
@@ -70,7 +106,7 @@ def _values_unequal(val1, val2, dec_scale, margin_scale=2):
 __pluginInfo__ = {
     'name': _CODE_NAME,
     'version': _RULE_VERSION,
-    'description': '''Assets should equal Liabilities and Shareholders Equity''',
-    #Mount points
+    'description': 'Assets should equal Liabilities and Shareholders Equity',
+    # Mount points
     'Validate.XBRL.Finally': assets_eq_liability_equity,
 }
