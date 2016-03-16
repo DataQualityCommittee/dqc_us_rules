123a0712b561f1f28daebefee767309ad7415eeb
Sage.Smith <sagesmith@LS2-00204.local>
2016-03-16 13:36:41 -0600

    Fix flake8 errors in autogenerated comments

--- dqc_us_rules/dqc_us_0015.py
+++ dqc_us_rules/dqc_us_0015.py
@@ -1,5 +1,5 @@
-# (c) Copyright 2015 - 2016, XBRL US Inc. All rights reserved.   
-# See license.md for license information.  
+# (c) Copyright 2015 - 2016, XBRL US Inc. All rights reserved.
+# See license.md for license information.
 # See PatentNotice.md for patent infringement notice.
 import csv
 import os
