5176a02af586e8649ba3241ce580b90c0d7b959a
Sage.Smith <sagesmith@LS2-00204.local>
2016-04-12 16:36:42 -0600

    Added unit test

---
+++ tests/unit_tests/test_dqc_us_0041.py
@@ -0,0 +1,21 @@
+import unittest
+from datetime import datetime
+from dqc_us_rules import dqc_us_0041
+from unittest.mock import Mock, patch
+import arelle.ModelXbrl
+
+
+class TestCaches(unittest.TestCase):
+    def test__cache_loader(self):
+        print("")
+
+    def test_cache_builder(self):
+        print("")
+
+    def test_cache_setup(self):
+        print("")
+
+
+class TestDefaultDimensions(unittest.TestCase):
+    def test_catch_0041_errors(self):
+        print("")
