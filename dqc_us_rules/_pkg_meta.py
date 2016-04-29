124bd74ab2ce532385c93fcff66bb71c72a040bb
Andrew Perkins <andrew.perkins@workiva.com>
2016-04-29 15:36:21 -0600

    RC 1 Release version update

--- dqc_us_rules/_pkg_meta.py
+++ dqc_us_rules/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 This module contains the revision information for the dqc_us_rules library.
 """
-version_info = (1, 0, 8)
+version_info = (2, 0, 0, '-RC1')
 version = '.'.join(map(str, version_info))
