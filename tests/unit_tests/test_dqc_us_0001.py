ec37d9ff3def285e6717b1d206bc1457f28225d5
Jay Leinbach <jason.leinbach@webfilings.com>
2016-05-04 10:49:31 -0600

    Add tests for all_concepts_under.  Refs XS-3026

--- tests/unit_tests/test_dqc_us_0001.py
+++ tests/unit_tests/test_dqc_us_0001.py
@@ -30,3 +30,84 @@ class TestIsDomain(unittest.TestCase):
 
         concept.qname = Mock(localName='MikeDomain')
         self.assertTrue(dqc_us_0001._is_domain(concept))
+
+class TestAllConceptsUnder(unittest.TestCase):
+    root_concept = None
+    relset = []
+    tree_concepts = []
+
+    def setUp(self):
+        """
+        Initialize a relset with this tree:
+            A
+            |-- B
+            |    +-- C
+            |         +-- A
+            |              +-- (...)  # cycle
+            +-- D
+        """
+        # make some concepts
+        cncpt_a = Mock()  # root
+        cncpt_b = Mock()
+        cncpt_c = Mock()
+        cncpt_d = Mock()
+        self.tree_concepts = [cncpt_a, cncpt_b, cncpt_c, cncpt_d]
+        # initialize relset
+        mock_relset = Mock()
+        fromModelObject_rels = {
+            cncpt_a: [self._mock_rel(cncpt_a, cncpt_b), self._mock_rel(cncpt_a, cncpt_d)],
+            cncpt_b: [self._mock_rel(cncpt_b, cncpt_c)],
+            cncpt_c: [self._mock_rel(cncpt_c, cncpt_a)],  # cycle
+            cncpt_d: []
+        }
+        toModelObject_rels = {
+            cncpt_a: [self._mock_rel(cncpt_c, cncpt_a)],
+            cncpt_b: [self._mock_rel(cncpt_a, cncpt_b)],
+            cncpt_c: [self._mock_rel(cncpt_b, cncpt_c)],
+            cncpt_d: [self._mock_rel(cncpt_a, cncpt_d)]
+        }
+        mock_relset.fromModelObject = lambda c: fromModelObject_rels[c]
+        mock_relset.toModelObject = lambda c: toModelObject_rels[c]
+        self.root_concept = cncpt_a
+        self.relset = mock_relset
+
+    @staticmethod
+    def _mock_rel(fr, to):
+        rel = Mock()
+        rel.fromModelObject = fr
+        rel.toModelObject = to
+        rel.toLocator = id(to)
+        return rel
+
+    @patch('dqc_us_rules.dqc_us_0001._is_domain', return_value=False)
+    @patch('dqc_us_rules.dqc_us_0001._is_concept', return_value=True)
+    def test_traversal(self, _, __):
+        """
+        Test relationshipSet traversal
+        """
+        concepts = dqc_us_0001._all_members_under(self.root_concept, self.relset)
+        # check that we got every concept under the tree
+        expected_concepts = set(self.tree_concepts)
+        self.assertEqual(set(concepts), expected_concepts)
+
+    @patch('dqc_us_rules.dqc_us_0001._is_domain', return_value=True)
+    @patch('dqc_us_rules.dqc_us_0001._is_concept', return_value=True)
+    def test_traversal_all_domains(self, _, __):
+        """
+        Test relationshipSet traversal
+        """
+        concepts = dqc_us_0001._all_members_under(self.root_concept, self.relset)
+        # check that we got every concept under the tree
+        expected_concepts = set()
+        self.assertEqual(set(concepts), expected_concepts)
+
+    @patch('dqc_us_rules.dqc_us_0001._is_domain', return_value=False)
+    @patch('dqc_us_rules.dqc_us_0001._is_concept', return_value=False)
+    def test_traversal_no_concepts(self, _, __):
+        """
+        Test relationshipSet traversal
+        """
+        concepts = dqc_us_0001._all_members_under(self.root_concept, self.relset)
+        # check that we got every concept under the tree
+        expected_concepts = set()
+        self.assertEqual(set(concepts), expected_concepts)
