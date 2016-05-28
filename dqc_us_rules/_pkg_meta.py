938b532ba4b825e3d155a6490b0ba26bc2b055e4
Minde.Erickson <minde.erickson@workiva.com>
2016-05-27 18:49:19 -0600

    Update version to RC7

--- dqc_us_rules/_pkg_meta.py
+++ dqc_us_rules/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 This module contains the revision information for the dqc_us_rules library.
 """
-version_info = (2, 0, 0, 'RC6')
+version_info = (2, 0, 0, 'RC7')
 version = '.'.join(map(str, version_info))
