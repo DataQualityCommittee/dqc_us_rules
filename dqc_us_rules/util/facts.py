4324b64a396cbd2f9d73baa57d3bf7c83150d47d
David Tauriello <david.tauriello@xbrl.us>
2016-06-21 13:21:49 -0400

    update files with 2016 details

--- dqc_us_rules/util/facts.py
+++ dqc_us_rules/util/facts.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2015, Workiva Inc.  All rights reserved
-# Copyright (c) 2015, XBRL US Inc.  All rights reserved
+# Copyright (c) 2015-2016, Workiva Inc.  All rights reserved.
+# Copyright (c) 2015-2016, XBRL US Inc.  All rights reserved.
 from collections import defaultdict
 from arelle import ValidateXbrlCalcs
 
