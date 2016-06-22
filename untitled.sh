9d1d2f9f74ae1e97ac6946062d59268830e1e174
Jay Leinbach <jason.leinbach@webfilings.com>
2016-06-22 13:08:37 -0600

    Another poke at travis.  Refs XS-3172

--- untitled.sh
+++ untitled.sh
@@ -4,5 +4,6 @@ TESTCASESROOT=./DQC_Testcases_v66
 OUTPUTLOGFILE=./DQC-v70-log.xml
 OUTPUTERRFILE=./DQC-v70-err.txt
 OUTPUTCSVFILE=./DQC-v70-report.csv
+CURRDIR=$(pwd)
 TESTCASESINDEXFILE="$TESTCASESROOT/index.xml"
-python3.4 -m arelle.CntlrCmdLine --file "$TESTCASESINDEXFILE" --validate --plugins './dqc_us_rules/dqc_us_rules|validate/EFM|logging/dqcParameters.py' --csvTestReport "$OUTPUTCSVFILE"  --logFile "$OUTPUTLOGFILE" --disclosureSystem efm-pragmatic-all-years --logCodeFilter '(?!EFM)' 2>  "$OUTPUTERRFILE"
+python3.4 -m arelle.CntlrCmdLine --file "$TESTCASESINDEXFILE" --validate --plugins '$CURRDIR/dqc_us_rules|validate/EFM|logging/dqcParameters.py' --csvTestReport "$OUTPUTCSVFILE"  --logFile "$OUTPUTLOGFILE" --disclosureSystem efm-pragmatic-all-years --logCodeFilter '(?!EFM)' 2>  "$OUTPUTERRFILE"
