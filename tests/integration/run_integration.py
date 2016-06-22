5d44d1a52d3078b44d9e732c3f24f6a9da9f9587
Jay Leinbach <jason.leinbach@webfilings.com>
2016-06-22 14:47:13 -0600

    Complete first pass at integration runner.  Refs XS-3172

---
+++ tests/integration/run_integration.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import sys
+
+_EXCLUDED_TESTS = [
+    'DQC_0001.58.2593',
+    'DQC_0001.60.3062',
+    'DQC_0001.64.3058',
+    'DQC_0001.65.3057',
+    'DQC_0001.72.3052',
+    'DQC_0018.34.3452',
+    'DQC_0018.34.2781'
+]
+
+_REPORT_OUTPUT_FILE = os.path.join(
+    os.path.dirname(__file__),
+    '..',
+    '..',
+    'DQC-v70-report.csv'
+)
+
+
+def main():
+    with open(_REPORT_OUTPUT_FILE, 'rt') as f:
+        reader = csv.reader(f)
+        #skip header
+        next(reader, None)
+        failed_rows = []
+        for row in reader:
+            if len(row) > 5:
+                if row[2] not in _EXCLUDED_TESTS:
+                    if row[5] != 'pass':
+                        failed_rows.append(row)
+    if len(failed_rows) > 0:
+        print('Tests failed: {}'.format(failed_rows))
+        sys.exit(1)
+    else:
+        print('All included tests pass')
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
--- untitled.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run DQC Conformance Suite tests
-TESTCASESROOT=./DQC_Testcases_v66
-OUTPUTLOGFILE=./DQC-v70-log.xml
-OUTPUTERRFILE=./DQC-v70-err.txt
-OUTPUTCSVFILE=./DQC-v70-report.csv
-CURRDIR=$(pwd)
-TESTCASESINDEXFILE="$TESTCASESROOT/index.xml"
-python3.4 -m arelle.CntlrCmdLine --file "$TESTCASESINDEXFILE" --validate --plugins "$CURRDIR/dqc_us_rules|validate/EFM|logging/dqcParameters.py" --csvTestReport "$OUTPUTCSVFILE"  --logFile "$OUTPUTLOGFILE" --disclosureSystem efm-pragmatic-all-years --logCodeFilter '(?!EFM)' 2>  "$OUTPUTERRFILE"
