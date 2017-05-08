f5f55007ad039d9388114c7b4f46035be32f2cb6
allenbross-wf <allen.bross@workiva.com>
2017-05-08 13:38:08 -0600

    Removed excess code and added message to message file.

--- dqc_us_rules/dqc_us_0046.py
+++ dqc_us_rules/dqc_us_0046.py
@@ -3,14 +3,13 @@
 # See https://xbrl.us/dqc-patent for patent infringement notice.
 import json
 import os
-from arelle import XbrlConst, ModelXbrl
+from arelle import XbrlConst
 from .util import messages
-from collections import defaultdict, OrderedDict
-from arelle.FileSource import saveFile, openFileSource
+from collections import defaultdict
 
 _CODE_NAME = 'DQC.US.0046'
 _RULE_VERSION = '3.1.0'
-_DQC_08_CALC_FILE = os.path.join(
+_DQC_46_CALC_FILE = os.path.join(
     os.path.dirname(__file__),
     'resources',
     'DQC_US_0046',
@@ -40,24 +39,18 @@ def _run_checks(val):
     checked_axes = defaultdict(list)
     config_json_file = _CONFIG_JSON_FILE
     calc_children = _load_config(config_json_file)
-
-
     if not calc_children:
             return  # nothing can be checked
-
     # convert children lists into sets for faster "in" function processing
     calc_children = dict((k, set(v)) for k, v in calc_children.items())
     calc_rels = val.modelXbrl.relationshipSet(
         XbrlConst.summationItem).modelRelationships
     for rel in calc_rels:
         calc_child_rels = calc_children.get(
-            rel.toModelObject.qname.localName,
+            rel.fromModelObject.qname.localName,
             _EMPTY_LIST
         )
-        print(calc_children)
-        #print(calc_child_rels)
-        #print(rel.fromModelObject.qname.localName)
-        if rel.fromModelObject.qname.localName in calc_child_rels:
+        if rel.toModelObject.qname.localName in calc_child_rels:
             # ugt has reversed relationship
             val.modelXbrl.error(
                 '{base_key}.{extension_key}'.format(
@@ -69,11 +62,7 @@ def _run_checks(val):
                 extCalcTargetName=rel.toModelObject.label(),
                 ruleVersion=_RULE_VERSION
             )
-            print(
-                "extension parent " + rel.fromModelObject.qname.localName +
-                " and child " + rel.toModelObject.qname.localName +
-                " reversed from ugt"
-            )
+
 
 def _load_config(calcs_file):
     """
