032a4000b98a8e17a39e2203aa9ec033959f7d75
allenbross-wf <allen.bross@workiva.com>
2017-07-21 18:50:54 -0600

    Incremented release number.

--- dqc_us_rules/_pkg_meta.py
+++ dqc_us_rules/_pkg_meta.py
@@ -1,5 +1,5 @@
 """
 This module contains the revision information for the dqc_us_rules library.
 """
-version_info = (5, 0, 0, 'RC2')
+version_info = (5, 0, 0, 'RC3')
 version = '.'.join(map(str, version_info))
