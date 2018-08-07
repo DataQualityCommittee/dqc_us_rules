293c955f765c12a55230e0ac3edb698ba581f249
phillipengel <phillip@goffengel.com>
2018-08-06 20:55:23 -0700

    Update travis-run.sh

--- travis-run.sh
+++ travis-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $INFILES > infiles.json
-python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml
+python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml --xule-run
