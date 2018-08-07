caeafa7c0d46203b4d31ad04d6444b42248b43b6
Phillip Engel <phillip@goffengel.com>
2018-08-07 12:55:38 -0700

    add back call to arelle

--- travis-run.sh
+++ travis-run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -x
 echo $INFILES > infiles.json
-echo $CURDIR
 sed -i "s|LOCATION|$CURDIR/dqc_us_rules|" $CURDIR/plugin/xule/rulesetMap.json
-cat $CURDIR/plugin/xule/rulesetMap.json
 cp -r $CURDIR/plugin/xule /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/
 cp $CURDIR/plugin/validate/DQC.py /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/validate/
-
-#python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml --xule-run
+python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml -v
