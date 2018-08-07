8dcaf229946e56636da8818b0673e07eb09868f6
Phillip Engel <phillip@goffengel.com>
2018-08-07 13:02:53 -0700

    output log file

--- travis-run.sh
+++ travis-run.sh
@@ -5,3 +5,4 @@ sed -i "s|LOCATION|$CURDIR/dqc_us_rules|" $CURDIR/plugin/xule/rulesetMap.json
 cp -r $CURDIR/plugin/xule /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/
 cp $CURDIR/plugin/validate/DQC.py /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/validate/
 python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml -v
+echo '===== START LOG FILE =====' && cat log.xml && echo '===== END LOG FILE ====='
