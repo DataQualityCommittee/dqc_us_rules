aea23cb6e6da72d5172564c6b04cab6333f09f91
phillipengel <phillip@goffengel.com>
2018-08-06 20:27:02 -0700

    fix quotes

--- travis-run.sh
+++ travis-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list '$INFILES' --logFile log.xml
+python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list $INFILES --logFile log.xml
