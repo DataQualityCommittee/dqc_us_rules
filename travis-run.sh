3e39efbf607ceca9288b1303388a8bd3e39b36ef
Phillip Engel <phillip@goffengel.com>
2018-08-07 12:45:21 -0700

    Update ruleset map file to point to the loacl rulesets

--- travis-run.sh
+++ travis-run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -x
 echo $INFILES > infiles.json
-python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml --xule-run
+sed -i "s|LOCATION|$CURDIR/dqc_us_rules|" $CURDIR/plugin/xule/rulesetMap.json
+cat $CURDIR/plugin/xule/rulesetMap.json
+cp -r $CURDIR/plugin/xule /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/
+cp $CURDIR/plugin/validate/DQC.py /home/travis/virtualenv/python3.5.5/src/arelle/arelle/plugin/validate/
+
+#python3.5 -m arelle.CntlrCmdLine --plugin validate/DQC -v --xule-filing-list infiles.json --logFile log.xml --xule-run
