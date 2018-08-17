ee54b5f1d85fbb608edf54c4d728decff28e6739
Phillip Engel <phillip@goffengel.com>
2018-08-17 09:46:38 -0700

    test fix python location

--- travis-run.sh
+++ travis-run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -x
-env
 echo $INFILES > infiles.json
 sed -i "s|https://github.com/DataQualityCommittee/dqc_us_rules/.*/dqc_us_rules/|dqc_us_rules/|" $CURDIR/plugin/xule/rulesetMap.json
 sed -i "s|\?raw=true||" $CURDIR/plugin/xule/rulesetMap.json
-cp -r $CURDIR/plugin/xule $HOME/virtualenv/$TRAVIS_PYTHON_VERSION/src/arelle/arelle/plugin/
-cp $CURDIR/plugin/validate/DQC.py $HOME/virtualenv/$TRAVIS_PYTHON_VERSION/src/arelle/arelle/plugin/validate/
+cp -r $CURDIR/plugin/xule $VIRTUAL_ENV/src/arelle/arelle/plugin/
+cp $CURDIR/plugin/validate/DQC.py $VIRTUAL_ENV/src/arelle/arelle/plugin/validate/
 python3.5 -m arelle.CntlrCmdLine --plugin "validate/DQC|transforms/SEC" -v --xule-filing-list infiles.json --logFile log.xml -v --xule-debug
 python3.5 compare.py --compare-file report.txt --test-files log.xml --expected-results $EXFILES 
