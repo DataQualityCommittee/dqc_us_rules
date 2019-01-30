59ef6efe5e150621cd59d1702ac622de9918e79f
David Tauriello <david.tauriello@xbrl.us>
2019-01-30 17:07:21 -0500

    update xule effective v7.1

--- plugin/xule/XuleRollForward.py
+++ plugin/xule/XuleRollForward.py
@@ -3,7 +3,7 @@ Xule is rule processor for XBRL (X)brl r(ULE).
 
 Copyright (c) 2014 XBRL US Inc. All rights reserved
 
-$Change: 22614 $
+$Change: 22653 $
 '''
 import collections
 import re
@@ -698,6 +698,7 @@ def built_in_functions():
     return funcs
 
 BUILTIN_FUNCTIONS = built_in_functions()
+
 
 
 
