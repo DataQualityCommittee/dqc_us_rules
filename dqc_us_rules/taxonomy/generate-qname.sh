99268626fd84ca84748042c34847fcea054d1e3f
Campbell <campbell.pryde@xbrl.us>
2021-01-18 16:39:17 -0500

    Added 2021 rules
    Added 2021 rules to the DQC ruleset

--- dqc_us_rules/taxonomy/generate-qname.sh
+++ dqc_us_rules/taxonomy/generate-qname.sh
@@ -9,6 +9,10 @@ python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qname
 python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qnames-dir '/Users/campbellpryde/Documents/GitHub/filer_membership/taxonomies/IFRS/2018/' --xule-qnames-format json -f 'http://xbrl.ifrs.org/taxonomy/2018-03-16/full_ifrs_entry_point_2018-03-16.xsd'
 
 
+python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qnames-dir '/Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/taxonomy/us/2021/' --xule-qnames-format json -f 'http://xbrl.fasb.org/us-gaap/2021/entire/us-gaap-entryPoint-std-2021-01-31.xsd'
+
+python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qnames-dir '/Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/taxonomy/us/2021/' --xule-qnames-format json -f 'https://xbrl.fasb.org/us-gaap/2021/dqcrules/dqcrules-2021-01-31.xsd'
+
 python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qnames-dir '/Users/campbellpryde/Documents/GitHub/filer_membership/taxonomies/2020/' --xule-qnames-format json -f 'http://xbrl.fasb.org/us-gaap/2020/dqcrules/dqcrules-2020-01-31.xsd'
 
 python3 Arelle-master/arellecmdline.py --plugin xule/savexuleqnames --xule-qnames-dir '/Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/taxonomy/us/2017/' --xule-qnames-format json -f 'http://xbrl.fasb.org/us-gaap/2017/entire/us-gaap-entryPoint-std-2017-01-31.xsd'
