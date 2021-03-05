f86935731904aa3801d0651156e844aaeb5d8baa
David Tauriello <david.tauriello@xbrl.us>
2021-03-05 17:20:37 -0500

    resolve conflicts for v14 merge

--- plugin/validate/DQC.py
+++ plugin/validate/DQC.py
@@ -20,7 +20,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-$Change: 22775 $
+$Change: 22782 $
 DOCSKIP
 """
 import optparse
@@ -36,17 +36,16 @@ _license = 'Apache-2'
 _author = 'XBRL US Inc.'
 _copyright = '(c) 2017-2018'
 _rule_set_map_name = 'rulesetMap.json'
-_latest_map_name = 'https://raw.githubusercontent.com/DataQualityCommittee/dqc_us_rules/master/plugin/xule/rulesetMap.json?raw=true'
+_latest_map_name = 'https://raw.githubusercontent.com/DataQualityCommittee/dqc_us_rules/master/plugin/xule/rulesetMap.json?raw=true' 
 
 """Do not change anything below this line."""
 _xule_plugin_info = None
 
 def cmdOptions(parser):
     """Extend command line options for xule validator
-
+    
     This is called by the Arelle controller.
     """
-
     if isinstance(parser, optparse.OptionParser):
         # This is the normal optparse.OptionsParser object.
         parserGroup = optparse.OptionGroup(parser,
@@ -67,14 +66,14 @@ def cmdOptions(parser):
     parserGroup.add_option("--{}-display-rule-set-map".format(_short_name).lower(),
                            action="store_true",
                            dest="{}_display_rule_set_map".format(_short_name),
-                           help=_("Display the rule set map currently used."))
+                           help=_("Display the rule set map currently used."))   
 
     # Update validator rule set map
     parserGroup.add_option("--{}-update-rule-set-map".format(_short_name).lower(),
                            action="store",
                            dest="{}_update_rule_set_map".format(_short_name),
                            help=_("Update the rule set map currently used. The supplied file will be merged with the current rule set map."))
-
+    
     # Replace validator rule set map
     parserGroup.add_option("--{}-replace-rule-set-map".format(_short_name).lower(),
                            action="store",
@@ -97,11 +96,15 @@ def cmdOptions(parser):
 
 def cntrlrCmdLineUtilityRun(cntlr, options, **kwargs):
     """Validator run utility.
-
+    
     This is invoked by the Arelle controler after Arelle is fully up but before a filing is loaded.
     """
-    parser = optparse.OptionParser()
+    # Save options in xule
+    save_options_method = getXuleMethod(cntlr, 'Xule.CntrlCmdLine.Utility.Run.Init')
+    save_options_method(cntlr, options, **kwargs)
 
+    parser = optparse.OptionParser()
+    
     # Check that both update an replace rule set map are not used together.
     replace_update_rule_set_map_options = [x.lower().replace('_','-') for x in ('{}_update_rule_set_map'.format(_short_name),
                                                        '{}_replace_rule_set_map'.format(_short_name),
@@ -116,23 +119,25 @@ def cntrlrCmdLineUtilityRun(cntlr, options, **kwargs):
     #if len([x for x in (getattr(options, "{}_update_rule_set_map".format(_short_name), False),
     #                   getattr(options, "{}_replace_rule_set_map".format(_short_name), False)) if x]) > 1:
     #    parser.error(_("Cannot use --{short_name}-update-rule-set-map and --{short_name}-replace-rule-set-map the same time.".format(short_name=_short_name)))
-
+    
     # Show validator version
     if getattr(options, '{}_version'.format(_short_name), False):
         version_method = getXuleMethod(cntlr, 'Xule.ValidatorVersion')
-        cntlr.addToLog("{} validator version: {}".format(_short_name,  _version_prefix + version_method(__file__)), _short_name)
-        cntlr.close()
+        version_method(cntlr, _short_name, _rule_set_map_name, _version_prefix, __file__)
 
+        #cntlr.addToLog("{} validator version: {}".format(_short_name,  _version_prefix + version_method(__file__)), _short_name)
+        #cntlr.close()
+    
     # Update the rule set map
     if getattr(options, "{}_update_rule_set_map".format(_short_name), False):
         update_method = getXuleMethod(cntlr, 'Xule.RulesetMap.Update')
         update_method(cntlr, getattr(options,"{}_update_rule_set_map".format(_short_name)), _rule_set_map_name)
-
+    
     # Replace the rule set map
     if getattr(options, "{}_replace_rule_set_map".format(_short_name), False):
         update_method = getXuleMethod(cntlr, 'Xule.RulesetMap.Replace')
         update_method(cntlr, getattr(options,"{}_replace_rule_set_map".format(_short_name)), _rule_set_map_name)
-
+    
     # Display the rule set map
     if getattr(options, "{}_display_rule_set_map".format(_short_name), False):
         update_method = getXuleMethod(cntlr, 'Xule.RulesetMap.Display')
@@ -151,10 +156,10 @@ def cntrlrCmdLineUtilityRun(cntlr, options, **kwargs):
     # Register the xule validator
     registerMethod = getXuleMethod(cntlr, 'Xule.RegisterValidator')
     registerMethod(_short_name, _rule_set_map_name)
-
+    
 def getXulePlugin(cntlr):
     """Find the Xule plugin
-
+    
     This will locate the Xule plugin module.
     """
     global _xule_plugin_info
@@ -165,24 +170,24 @@ def getXulePlugin(cntlr):
                 break
         else:
             cntlr.addToLog(_("Xule plugin is not loaded. Xule plugin is required to run DQC rules. This plugin should be automatically loaded."))
-
+    
     return _xule_plugin_info
 
 def getXuleMethod(cntlr, class_name):
     """Get method from Xule
-
+    
     Get a method/function from the Xule plugin. This is how this validator calls functions in the Xule plugin.
     """
     return getXulePlugin(cntlr).get(class_name)
 
 def menuTools(cntlr, menu):
     """Add validator menu the Tools menu in the Arelle GUI
-
+    
     This is invoked by the Arelle controller
     """
     menu_method = getXuleMethod(cntlr, 'Xule.AddMenuTools')
     version_method = getXuleMethod(cntlr, 'Xule.ValidatorVersion')
-    menu_method(cntlr, menu, _short_name, _version_prefix + version_method(__file__), _rule_set_map_name, _latest_map_name)
+    menu_method(cntlr, menu, _short_name, _version_prefix, __file__, _rule_set_map_name, _latest_map_name)
 
 def validateMenuTools(cntlr, validateMenu, *args, **kwargs):
     """Add validator checkbutton to the Arelle Validate menu (under Tools).
