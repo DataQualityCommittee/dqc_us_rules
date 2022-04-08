65c76f0429433d43750b4a791102b193f335aa60
David Tauriello <david.tauriello@xbrl.us>
2022-04-08 15:34:34 -0400

    Update travis to use rulesetMap in root of repository

--- travis-run.sh
+++ travis-run.sh
@@ -5,9 +5,9 @@ svn export --quiet https://github.com/xbrlus/xule/trunk/plugin/validate $CURDIR/
 cp -R $CURDIR/plugin/xule $VIRTUAL_ENV/src/arelle/arelle/plugin/xule
 cp $CURDIR/plugin/validate/DQC.py $VIRTUAL_ENV/src/arelle/arelle/plugin/validate/DQC.py
 echo $INFILES > infiles.json
-sed -i "s|https://github.com/DataQualityCommittee/dqc_us_rules/.*/dqc_us_rules/|$GH_SLUG/raw/$PR_BR/dqc_us_rules/|" $CURDIR/plugin/xule/rulesetMap.json
-sed -i "s|\?raw=true||" $CURDIR/plugin/xule/rulesetMap.json
+sed -i "s|https://github.com/DataQualityCommittee/dqc_us_rules/.*/dqc_us_rules/|$GH_SLUG/raw/$PR_BR/dqc_us_rules/|" $CURDIR/rulesetMap.json
+sed -i "s|\?raw=true||" $CURDIR/rulesetMap.json
 cat $CURDIR/plugin/xule/rulesetCompatibility.json
-python3.9 -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|transforms/SEC" -v --dqc-replace-rule-set-map $CURDIR/plugin/xule/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
-#cat /home/travis/.config/arelle/plugin/xule/rulesetMap.json
+python3.9 -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|transforms/SEC" -v --dqc-replace-rule-set-map $CURDIR/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
+cat /home/travis/.config/arelle/plugin/xule/rulesetMap.json
 python3.9 compare.py --compare-file $CURDIR/report.txt --test-files $CURDIR/log.xml --expected-results $EXFILES --html-file report.html
