4dccc1da66cd15bc1d35f91aa4a0165d03e49755
David Tauriello <david.tauriello@xbrl.us>
2022-11-23 15:57:17 -0500

    Update travis-run.sh
    - use python 3.9

--- travis-run.sh
+++ travis-run.sh
@@ -3,12 +3,12 @@ pip show Arelle-release
 set -x
 svn export --quiet https://github.com/xbrlus/xule/trunk/plugin/xule $CURDIR/plugin/xule
 svn export --quiet https://github.com/xbrlus/xule/trunk/plugin/validate $CURDIR/plugin/validate
-cp -R $CURDIR/plugin/xule $VIRTUAL_ENV/lib/python3.10/site-packages/arelle/plugin/xule
-cp $CURDIR/plugin/validate/DQC.py $VIRTUAL_ENV/lib/python3.10/site-packages/arelle/plugin/validate/DQC.py
+cp -R $CURDIR/plugin/xule $VIRTUAL_ENV/lib/python3.9/site-packages/arelle/plugin/xule
+cp $CURDIR/plugin/validate/DQC.py $VIRTUAL_ENV/lib/python3.9/site-packages/arelle/plugin/validate/DQC.py
 echo $INFILES > infiles.json
 sed -i "s|https://github.com/DataQualityCommittee/dqc_us_rules/.*/dqc_us_rules/|$GH_SLUG/raw/$PR_BR/dqc_us_rules/|" $CURDIR/rulesetMap.json
 sed -i "s|\?raw=true||" $CURDIR/rulesetMap.json
 cat $CURDIR/plugin/xule/rulesetCompatibility.json
-python3.10 -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|transforms/SEC" -v --dqc-replace-rule-set-map $CURDIR/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
+python3.9 -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|transforms/SEC" -v --dqc-replace-rule-set-map $CURDIR/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
 cat /home/travis/.config/arelle/plugin/xule/rulesetMap.json
-python3.10 compare.py --compare-file $CURDIR/report.txt --test-files $CURDIR/log.xml --expected-results $EXFILES --html-file report.html
+python3.9 compare.py --compare-file $CURDIR/report.txt --test-files $CURDIR/log.xml --expected-results $EXFILES --html-file report.html
