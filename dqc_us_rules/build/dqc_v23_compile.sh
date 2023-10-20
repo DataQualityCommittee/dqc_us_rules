ef10d8327cf9ffcc420fd3f36ebf6760895ab59e
Campbell <campbell.pryde@xbrl.us>
2023-10-20 15:00:11 -0400

    Create dqc_v23_compile.sh
    Added build script

---
+++ dqc_us_rules/build/dqc_v23_compile.sh
@@ -0,0 +1,54 @@
+
+###---------------------------------------------V23----------------------------------V23----------------------------------V23------------------------------------V23
+# COMPILE RULES FOR  V23
+###---------------------------------------------V23----------------------------------V23----------------------------------V23------------------------------------V23
+
+
+#COMPILE RULES FOR US IN GIT HUB V23
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/us/2020/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2020-V23-ruleset.zip --xule-crash  --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/us/2021/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2021-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/us/2022/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2022-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/us/2023/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2023-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+# Add Packages V23
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2020-V23-ruleset.zip
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2021-V23-ruleset.zip 
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2022-V23-ruleset.zip
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-us-2023-V23-ruleset.zip 
+
+
+# COMPILE RULES FOR IFRS V23 IN GIT HUB
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/ifrs/2020/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2020-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/ifrs/2021/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2021-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/ifrs/2022/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2022-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/ifrs/2023/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2023-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+# Add Packages IFRS V23
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2020-V23-ruleset.zip
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2021-V23-ruleset.zip
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2022-V23-ruleset.zip
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-add-packages /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/resources.zip --xule-rule-set  /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-ifrs-2023-V23-ruleset.zip 
+
+# COMPILE RULES FOR ESEF V23 IN GIT HUB
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/esef/esef-2022/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-esef-2022-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/esef/esef-2021/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-esef-2021-V23-ruleset.zip --xule-crash --xule-stack-size=20
+
+python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins xule --xule-compile /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/source/esef/esef-2020/ --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule.dqc/dqc_us_rules/dqc-esef-2020-V23-ruleset.zip --xule-crash --xule-stack-size=20
