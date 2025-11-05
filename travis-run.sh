91a60aabb4c5588f8329b757a3feb6ca65da2ff2
David Tauriello <david.tauriello@xbrl.us>
2025-11-05 17:14:39 -0500

    Update .travis.yml and travis-run.sh
    - Python 3.13 | Arelle 2.37.58 | XULE 30050
    - Update travis-run for inlineXbrlDocumentSet

--- travis-run.sh
+++ travis-run.sh
@@ -18,6 +18,6 @@ echo $INFILES > infiles.json
 sed -i "s|https://github.com/DataQualityCommittee/dqc_us_rules/.*/dqc_us_rules/|$GH_SLUG/raw/$PR_BR/dqc_us_rules/|" $CURDIR/rulesetMap.json
 sed -i "s|\?raw=true||" $CURDIR/rulesetMap.json
 cat $CURDIR/plugin/xule/version.json
-$PYTHON_VERSION -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|EDGAR/transform" -v --dqc-replace-rule-set-map $CURDIR/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
+$PYTHON_VERSION -m arelle.CntlrCmdLine --httpUserAgent "DQC-Arelle (xbrl.us; dqc@xbrl.us)" --plugin "validate/DQC|EDGAR/transform|inlineXbrlDocumentSet" -v --dqc-replace-rule-set-map $CURDIR/rulesetMap.json --xule-filing-list infiles.json --logFile $CURDIR/log.xml -v --xule-debug
 cat /home/travis/.config/arelle/plugin/xule/rulesetMap.json
 $PYTHON_VERSION compare.py --compare-file $CURDIR/report.txt --test-files $CURDIR/log.xml --expected-results $EXFILES --html-file report.html
